f85ef518fce13ae3b263a5b99c3fa38f4a74808f
Stanislav Shtepa <s.shtepa@icontextgroup.ru>
2023-07-19 02:34:18 +0300

    + change release

--- src/APIWrapper.php
+++ src/APIWrapper.php
@@ -6,7 +6,7 @@ use ctapu4ok\VkMessengerSdk\Ips\Client;
 
 final class APIWrapper
 {
-    const RELEASE = '1.0.8';
+    const RELEASE = '1.0.9';
     private Client $API;
 
     public function logger(mixed $param, int $level = Logger::NOTICE, string $file = ''): void
